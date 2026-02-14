4cf869fb328f97808f9151d46edeeed8b2d3ead4
SilentSimulator <honeyclick123@gmail.com>
2026-02-14 22:54:54 +0530

    package

---
+++ package.sv
@@ -0,0 +1,12 @@
+package pb_pkg;
+
+  `include "inter.sv"
+  `include "trans.sv"
+  `include "gens.sv"
+  `include "dr.sv"
+  `include "mo.sv"
+  `include "sc.sv"
+  `include "en.sv"
+  `include "tes.sv"
+
+endpackage
