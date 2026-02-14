69db28b052c685086c49be0968e2d332b7e6422f
SilentSimulator <honeyclick123@gmail.com>
2026-02-14 22:53:39 +0530

    testbench for pbslave DUT with clock and reset

---
+++ testbench.sv
@@ -0,0 +1,40 @@
+module tb;
+
+  // Interface
+  apb_if intf();
+
+  // DUT
+  pbslave dut (
+    .pclk    (intf.pclk),
+    .presetn (intf.presetn),
+    .psel    (intf.psel),
+    .penable (intf.penable),
+    .pwrite  (intf.pwrite),
+    .paddr   (intf.paddr),
+    .pwdata  (intf.pwdata),
+    .prdata  (intf.prdata),
+    .pready  (intf.pready)
+  );
+
+  // Clock
+  initial begin
+    intf.pclk = 0;
+    forever #5 intf.pclk = ~intf.pclk;
+  end
+
+  // Reset
+  initial begin
+    intf.presetn = 0;
+    #20;
+    intf.presetn = 1;
+  end
+
+  // Test
+  pbtest test;
+
+  initial begin
+    test = new(intf);
+    test.run();
+  end
+
+endmodule
