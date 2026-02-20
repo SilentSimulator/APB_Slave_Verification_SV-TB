73f6e43b5e86cc97b2e69cacc38cc97dc5cf78f5
SilentSimulator <honeyclick123@gmail.com>
2026-02-20 23:46:37 +0530

    Fix syntax and formatting in transaction.sv

--- transaction.sv
+++ transaction.sv
@@ -4,12 +4,12 @@ class pbtrans;
   rand bit [31:0] pwdata;
   bit [31:0] prdata;
    
-  typedef enum { WRITE, READ} apb_op;
+  typedef enum bit{ WRITE, READ} apb_op;
   rand apb_op op;
   
   int unsigned tr_id;
   
-  constraint c{ paddr inside {[8'h00:8'ff]};}
+  constraint c{ paddr inside {[8'h00:8'hff]};}
   
   function new(int unsigned id = 0);
     tr_id = id;
@@ -17,8 +17,8 @@ class pbtrans;
   
   function void display();
     if(op == WRITE)
-      $display("[apb][write][id=%0d] paddr=%0hpwdata=%0h",tr_id,paddr,pwdata);
+      $display("[apb][write][id=%0d] paddr=%0h pwdata=%0h",tr_id,paddr,pwdata);
     else
-      $display("[apb][read][id=%0d] paddr=%0h,prdata=%0h",tr_id,paddr,prdata);
+      $display("[apb][read][id=%0d] paddr=%0h prdata=%0h",tr_id,paddr,prdata);
   endfunction
 endclass
