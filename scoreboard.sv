a6b5e8b812273f3e88ecf1369b93e83753fcab95
SilentSimulator <honeyclick123@gmail.com>
2026-02-20 23:50:07 +0530

    Refactor APB transaction handling in scoreboard

--- scoreboard.sv
+++ scoreboard.sv
@@ -4,7 +4,7 @@ class pbscb;
   parameter DATA_WIDTH = 32;
 
   mailbox mon2scb;
-  apb_trans tr;
+  pbtrans tr;
 
   logic [DATA_WIDTH-1:0] mem [0:(1<<ADDR_WIDTH)-1];
 
@@ -16,14 +16,14 @@ class pbscb;
     forever begin
       mon2scb.get(tr);
 
-      if (tr.pwrite) begin
-        mem[tr.addr] = tr.data;
+      if (tr.op == pbtrans::WRITE) begin
+        mem[tr.paddr] = tr.pwdata;
       end
       else begin
-        if (mem[tr.addr] != tr.data)
-          $display("ERROR at addr %0h", tr.addr);
+        if (mem[tr.paddr] != tr.prdata)
+          $display("ERROR at addr %0h", tr.paddr);
         else
-          $display("PASS at addr %0h", tr.addr);
+          $display("PASS at addr %0h", tr.paddr);
       end
     end
   endtask
